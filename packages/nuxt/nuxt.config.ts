9f9f23238efee97982ea56a0678ec5e7ebf18427
Michael Wallner <m.wallner@coderscantina.com>
2026-01-11 14:53:41 +0100

    🔥 remove Nuxt ESLint module and configs

--- packages/nuxt/nuxt.config.ts
+++ packages/nuxt/nuxt.config.ts
@@ -1,11 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
-  eslint: {
-    config: {
-      standalone: false,
-    },
-  },
   typescript: {
     tsConfig: {
       compilerOptions: {
